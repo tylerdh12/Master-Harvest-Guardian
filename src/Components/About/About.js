7f19195e014191502dc883ea84cab23328503da4
Tyler Harper <tylerdh12@gmail.com>
2020-03-04 20:16:04 -0800

    update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-// Bootstrap Imports
-import { Jumbotron, Button } from "react-bootstrap";
-
 export default function About(props) {
   return (
     <header className="masthead m-5">
       <div className="container rounded-corners neumorphic text-light h-100">
         <div className="row h-100 align-items-center">
           <div className="col-12 text-center">
-            <h1 className="font-weight-light p-4">
+            <h1 className="font-weight-light p-4 mt-5 mb-0">
               Vertically Centered Masthead Content
             </h1>
-            <p className="lead p-4">
+            <p className="lead p-4 mt-0">
               A great starter layout for a landing page
             </p>
           </div>
