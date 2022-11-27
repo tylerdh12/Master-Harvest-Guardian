e097d0c54c7301d8a1ef09da397039f03488e772
Tyler Harper <tyler@tylerharper.dev>
2022-11-27 12:58:26 -0800

    update package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
-
-// React Router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-// Component Import
 import Header from "./Components/Header";
 import FooterComp from "./Components/Footer";
 import Home from "./Routes/Home";
 import SignIn from "./Routes/SignIn";
 import SignUp from "./Routes/SignUp";
 
-/*
-Function App - Renders App Component to the DOM
-*/
 export default function App() {
   return (
     <Router>
       <div className="pageWrapper">
         <Header />
-        {/* A <Switch> looks through its children <Router>s and
-              renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/signup">
             <SignUp />
